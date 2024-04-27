cf22603e0815ec95a43127fc66a7ecfc7b52496a
maruf hossen <mdmdrufmeze@gmail.com>
2024-04-27 10:08:26 -0700

    add mongo divi

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,37 @@ app.use(express.json());
 
 
 
+
+const uri = "mongodb+srv://mdmarufmeze:m0xRoQzzkxLNb2D7@cluster0.a87xhva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.get('/', (req, res) =>{
     res.send("textile arts server is running")
 })
@@ -20,3 +52,10 @@ app.get('/', (req, res) =>{
 app.listen(port, ()=>{
     console.log(`texttile arts is running on port ${port}`)
 })
+
+
+
+
+
+// User Name: mdmarufmeze 
+// pass : m0xRoQzzkxLNb2D7
