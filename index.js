57cdb010ef88775ed6c9c976aa5f09439d5de2e3
maruf hossen <mdmdrufmeze@gmail.com>
2024-04-27 22:22:40 -0700

    add gitignor file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000;
@@ -12,11 +13,11 @@ app.use(express.json());
 
 
 
+console.log(process.env.DB_USER)
 
 
-
-const uri = "mongodb+srv://mdmarufmeze:m0xRoQzzkxLNb2D7@cluster0.a87xhva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a87xhva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +29,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+
+
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -35,7 +44,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -57,5 +66,3 @@ app.listen(port, ()=>{
 
 
 
-// User Name: mdmarufmeze 
-// pass : m0xRoQzzkxLNb2D7
