ec55e0cdab261a99741eef7b35a6e39591db3f49
maruf hossen <mdmdrufmeze@gmail.com>
2024-04-28 03:11:19 -0700

    add data

--- index.js
+++ index.js
@@ -29,16 +29,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+ // Connect the client to the server	(optional starting in v4.7)
+ await client.connect();
+
+const craftCollection = client.db('craftDB').collection('craft')
+
+app.post('/craft', async (req, res)=>{
+    const newCraft = req.body;
+    console.log(newCraft)
+    const result = await craftCollection.insertOne(newCraft);
+    res.send(result);
+})
 
 
 
 
 
 
-
-
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
