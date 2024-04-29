0a9bf7ac55d5367c53a25caa6e8f5f03b71296ad
maruf hossen <mdmdrufmeze@gmail.com>
2024-04-29 04:13:55 -0700

    add update craft

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ app.get('/craft', async(req, res)=>{
 
 app.get('/craft/:id', async(req, res) =>{
     const id =req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await craftCollection.findOne(query);
+    res.send(result);
 })
 
 app.post('/craft', async (req, res)=>{
